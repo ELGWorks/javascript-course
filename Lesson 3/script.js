13fdbb5370ad45f1802f0d2acd36e016523d3533
ELGWorks <244444991+ELGWorks@users.noreply.github.com>
2026-02-16 14:10:35 +0800

    Learning some more of operators and parenthesis

---
+++ Lesson 3/script.js
@@ -0,0 +1,24 @@
+// 'hello';
+// 'some' + 'text';
+// 'some' + 'more' + 'text';
+// '$' + (2095 + 799) / 100;
+// 'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100;
+// alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100 );
+// alert(`I'm learning JS`);
+// alert(`Items (${1 + 1}): $${(2095 + 799) / 100}`);
+// alert(`some
+// text`);
+
+
+//Exercises
+// alert(`My name is: `);
+// alert(`Emmanuel`);
+// alert(`My name is: ` + `Emmanuel`);
+// alert('Total cost: $' + (5 + 3));
+// alert(`Total cost: $${5 + 3}`);
+// alert('Total cost: $' + (599 + 295) / 100);
+// alert(`Total cost: $${(599 + 295) / 100}`);
+// alert(`Total cost: $${(599 + 295) / 100} \nThank you, come again!`);
+//Challenges
+// alert(`Items (${2 + 2}): $${(799 + 799 + 2095 + 2095) / 100} \nShipping & handling: $${(499 + 499) / 100} \nTotal before tax: $${(799 + 799 + 2095 + 2095 + 499 + 499) / 100} \nEstimated tax (10%): $${Math.round((799 + 799 + 2095 + 2095 + 499 + 499) * 0.1) / 100} \nOrder total: $${(Math.round((799 + 799 + 2095 + 2095 + 499 + 499) * 0.1) / 100) + ((799 + 799 + 2095 + 2095 + 499 + 499) / 100)}`);
+// alert(`Items (${1 + 1}): $${(799 + 2095) / 100} \nShipping & handling: $${(0 + 0) / 100} \nTotal before tax: $${(799 + 2095) / 100} \nEstimated tax (10%): $${Math.round((799 + 2095) * 0.1) / 100} \nOrder total: $${(Math.round((799 + 2095) * 0.1) + (799 + 2095)) / 100}`);
